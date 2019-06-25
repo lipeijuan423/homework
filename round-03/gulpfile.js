5164eb9d179890c24fa94d1deefce0e9ea4d9400
lipeijuan423 <1457216559@qq.com>
2019-06-25 23:38:41 +0800

    学习gulp

--- round-03/gulpfile.js
+++ round-03/gulpfile.js
@@ -1,1 +1,17 @@
 // server
+var gulp = require('gulp');
+var jsImport = require('gulp-js-import');
+var rollup = require('gulp-rollup');
+gulp.task('import', function () {
+    return gulp.src('./src/web/views/books/list.js').pipe(jsImport()).pipe(gulp.dest('dist'));
+})
+gulp.task('bundle', function (done) {
+    gulp.src('./src/**/*.js').pipe(rollup({
+        input: '' ,
+        output: {
+            format: 'es'
+        }
+    }))
+    .pipe(gulp.dest('./dist'));
+    done()
+})
--- round-03/webpack.config.js
+++ round-03/webpack.config.js
@@ -1,1 +1,16 @@
 // web
+const path = require('path');
+module.exports = {
+    entry: {
+        'index': './src/web/assets/scripts/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+            xTag: path.resolve(__dirname, 'node_modules/x-tag/dist/')
+        }
+    }
+}
