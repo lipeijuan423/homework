5ab08e7fb44404eb56c2ebad1b446a6be4896f1c
lipeijuan423 <1457216559@qq.com>
2019-06-28 23:04:49 +0800

    modify

--- round-03/gulpfile.js
+++
@@ -1,6 +0,0 @@
-// server
-var gulp = require('gulp');
-var jsImport = require('gulp-js-import');
-gulp.task('import', function () {
-    return gulp.src('./src/web/views/books/list.js').pipe(jsImport()).pipe(gulp.dest('dist'));
-})
